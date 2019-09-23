a6eab2b61f976ab1b477b9c07712a0e1c58e65b0
Owner <noran@danon.tzafonet.org.il>
2019-09-23 09:42:55 +0300

    fixed sitters and getters.

--- app/src/main/java/com/example/loreentaskmnger/SignIn.java
+++ app/src/main/java/com/example/loreentaskmnger/SignIn.java
@@ -1,10 +1,19 @@
 package com.example.loreentaskmnger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
     private EditText edPassword,edemail;
@@ -14,10 +23,69 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        edemail=(EditText)findViewById(R.id.edemail);
-        edPassword=(EditText)findViewById(R.id.edPassword);
-        btnsignin=(Button)findViewById(R.id.btnsignin);
-        btnsignup=(Button)findViewById(R.id.btnsignup);
+        edemail = (EditText) findViewById(R.id.edemail);
+        edPassword = (EditText) findViewById(R.id.edPassword);
+        btnsignin = (Button) findViewById(R.id.btnsignin);
+        btnsignup = (Button) findViewById(R.id.btnsignup);
 
+
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataHandler();
+            }
+        });
     }
+
+            private void dataHandler() {
+                String email =edemail .getText().toString();
+                String pasword= edPassword.getText().toString();
+                boolean isok=true;
+               // if (email.length()<4)
+             //  {
+                 //   edemail.setError("Email length Error");
+               //     isok=false;
+             //   }
+              //  if (email.indexOf("@")<0|| email.indexOf(".")<0) {
+               //     edemail.setError("email wrong format ");
+              //      isok = false;
+             //   }
+              //  if (pasword.length()<8)
+              //  {
+              //      edPassword.setError("password length error");
+                //    isok=false;
+             //   }
+                if (isValidEmailAddress(email)==false)
+                {
+                    edemail.setError("Invalid Email");
+                    isok=false;
+                }
+                if (isok)
+                {
+
+                }
+            }
+            public boolean isValidEmailAddress(String email) {
+                String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+                java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+                java.util.regex.Matcher m = p.matcher(email);
+                return m.matches();
+
+
+        }
+        private void singIn(String email , String password)
+        {
+            FirebaseAuth auth= FirebaseAuth.getInstance();
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(SignIn.this, " Sign successful.", Toast.LENGTH_SHORT);
+
+                    }
+                }
+            });
+        }
+
+
 }
