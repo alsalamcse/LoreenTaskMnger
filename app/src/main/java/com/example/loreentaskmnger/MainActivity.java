32efa98ce163fd34a4fea4bcd0b2a41420257637
Owner <noran@danon.tzafonet.org.il>
2019-10-08 12:21:30 +0300

    fixed sitters and getters.

--- app/src/main/java/com/example/loreentaskmnger/MainActivity.java
+++ app/src/main/java/com/example/loreentaskmnger/MainActivity.java
@@ -5,20 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onResume() {
+        super.onResume();
         Thread th=new Thread()
         {
             @Override
             public void run() {
                 try {
                     sleep(3*1000);
-                    Intent i=new Intent(getApplication(),SignIn.class);
-                    startActivity(i);
+                    FirebaseAuth auth=FirebaseAuth.getInstance();
+
+                    if (auth.getCurrentUser()==null|| auth.getCurrentUser().getEmail()==null)
+                    {
+                        Intent i=new Intent(getApplication(),SignIn.class);
+                        startActivity(i);
+                        finish();
+                    }
+                    else
+                    {
+                        Intent i=new Intent(getApplication(),MainTasksActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -26,5 +39,12 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         th.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
     }
 }
