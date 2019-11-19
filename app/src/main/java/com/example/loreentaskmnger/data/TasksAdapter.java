6a405297560fdc9e56a6f3ccd11843666f6c770a
Owner <noran@danon.tzafonet.org.il>
2019-11-19 12:26:40 +0200

    not complete , expcen

--- app/src/main/java/com/example/loreentaskmnger/data/TasksAdapter.java
+++ app/src/main/java/com/example/loreentaskmnger/data/TasksAdapter.java
@@ -3,6 +3,7 @@ package com.example.loreentaskmnger.data;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -17,24 +18,22 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.loreentaskmnger.MenuActivity;
 import com.example.loreentaskmnger.R;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 
-public abstract class TasksAdapter extends ArrayAdapter<MyTask>
-{
+public abstract class TasksAdapter extends ArrayAdapter<MyTask> {
 
-    public TasksAdapter(@NonNull Context context)
-    {
+    public TasksAdapter(@NonNull Context context) {
         super(context, R.layout.taskitem);
     }
 
-    public abstract TasksAdapter(@NonNull Context context);
+    //public abstract TasksAdapter(@NonNull Context context);
 
-    public abstract TasksAdapter(@NonNull Context context);
+    //public abstract TasksAdapter(@NonNull Context context);
 
     /**
-     *
      * @param position
      * @param convertView
      * @param parent
@@ -42,31 +41,27 @@ public abstract class TasksAdapter extends ArrayAdapter<MyTask>
      */
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
-    {
-        View vitem= LayoutInflater.from(getContext()).inflate(R.layout.taskitem,parent,false);
-        final TextView tvTitle=vitem.findViewById(R.id.itmTvTitle);
-        TextView tvSubject=vitem.findViewById(R.id.itmTvSubject);
-        RatingBar rbPrio=vitem.findViewById(R.id.itmRatingBar);
-        CheckBox chIsComplete=vitem.findViewById(R.id.itmChbxlsCompleted);
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View vitem = LayoutInflater.from(getContext()).inflate(R.layout.taskitem, parent, false);
+        final TextView tvTitle = vitem.findViewById(R.id.itmTvTitle);
+        TextView tvSubject = vitem.findViewById(R.id.itmTvSubject);
+        RatingBar rbPrio = vitem.findViewById(R.id.itmRatingBar);
+        CheckBox chIsComplete = vitem.findViewById(R.id.itmChbxlsCompleted);
         //getting data source
-        final ImageView ivInfo =vitem.findViewById(R.id.itmImageInfo);
+        final ImageView ivInfo = vitem.findViewById(R.id.itmImageInfo);
         final MyTask myTask = getItem(position);
 
         chIsComplete.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked)
-                {
+                if (isChecked) {
                     //todo delete this item
                     FirebaseUtils.getReference().child(myTask.getKey()).removeValue(new DatabaseReference.CompletionListener() {
                         @Override
                         public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
-                            if (databaseError==null)
-                            {
+                            if (databaseError == null) {
                                 Toast.makeText(getContext(), "delete", Toast.LENGTH_SHORT).show();
-                            }
-                            else{
+                            } else {
                                 Toast.makeText(getContext(), "not delete", Toast.LENGTH_SHORT).show();
                             }
                         }
@@ -75,23 +70,33 @@ public abstract class TasksAdapter extends ArrayAdapter<MyTask>
 
             }
         });
-
         ivInfo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-               //PopupMenu popup = new PopupMenu(TasksAdapter.this,ivInfo);
-              //  popup.getMenuInflater().inflate(R.menu.);
-                Toast.makeText(getContext(), "sucessful", Toast.LENGTH_SHORT).show();
-            }
+          public void onClick(View v) {
+               PopupMenu popupMenu = new PopupMenu(TasksAdapter.this, ivInfo);
+              popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+
+               popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                   @Override
+                   public boolean onMenuItemClick(MenuItem item) {
+                       Toast.makeText(TasksAdapter.this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                      return true;
+                   }
+               });
+                popupMenu.show();
+           }
         });
-        //connect item view to data source
-        tvTitle.setText(myTask.getTittle());
-        tvSubject.setText(myTask.getSubject());
-        rbPrio.setRating(myTask.getImportant());
-        chIsComplete.setChecked(false);
-        return vitem;
     }
 
-    protected abstract void onCreate(Bundle savedInstanceState);
+
+       
+        {
+            
+            tvTitle.setText(myTask.getTittle());
+            tvSubject.setText(myTask.getSubject());
+            rbPrio.setRating(myTask.getImportant());
+            chIsComplete.setChecked(false);
+            return vitem;
+        }
+    }
 }
