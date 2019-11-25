20fda7e49eb21f515f4efa0ee26527a4bea454aa
Owner <noran@danon.tzafonet.org.il>
2019-11-25 09:00:35 +0200

    menu

--- app/src/main/java/com/example/loreentaskmnger/data/TasksAdapter.java
+++ app/src/main/java/com/example/loreentaskmnger/data/TasksAdapter.java
@@ -1,16 +1,15 @@
 package com.example.loreentaskmnger.data;
 
+import android.app.AlertDialog;
 import android.content.Context;
-import android.os.Bundle;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.PopupMenu;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,12 +17,11 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.example.loreentaskmnger.MenuActivity;
 import com.example.loreentaskmnger.R;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 
-public abstract class TasksAdapter extends ArrayAdapter<MyTask> {
+public class TasksAdapter extends ArrayAdapter<MyTask> {
 
     public TasksAdapter(@NonNull Context context) {
         super(context, R.layout.taskitem);
@@ -72,31 +70,51 @@ public abstract class TasksAdapter extends ArrayAdapter<MyTask> {
         });
         ivInfo.setOnClickListener(new View.OnClickListener() {
             @Override
-          public void onClick(View v) {
-               PopupMenu popupMenu = new PopupMenu(TasksAdapter.this, ivInfo);
-              popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
-
-               popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                   @Override
-                   public boolean onMenuItemClick(MenuItem item) {
-                       Toast.makeText(TasksAdapter.this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
-                      return true;
-                   }
-               });
-                popupMenu.show();
-           }
+            public void onClick(View v)
+            {
+                Toast.makeText(getContext(), myTask.getTittle(), Toast.LENGTH_SHORT).show();
+                ShowMenu();
+            }
         });
-    }
 
 
-       
-        {
-            
+
             tvTitle.setText(myTask.getTittle());
             tvSubject.setText(myTask.getSubject());
             rbPrio.setRating(myTask.getImportant());
             chIsComplete.setChecked(false);
             return vitem;
         }
+        public void ShowMenu ()
+    {
+            final String[] option = {"Add", "View", "Select", "Delete"};
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.select_dialog_item);
+            adapter.addAll(option);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setTitle("Select option");
+            builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    if (i == 0) {
+                        Toast.makeText(getContext(), "Add", Toast.LENGTH_SHORT).show();
+                    }
+                    if (i == 1) {
+                        Toast.makeText(getContext(), "View", Toast.LENGTH_SHORT).show();
+                    }
+                    if (i == 2) {
+                        Toast.makeText(getContext(), "Select", Toast.LENGTH_SHORT).show();
+                    }
+                    if (i == 3) {
+                        Toast.makeText(getContext(), "Delete", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }
+            });
+            final AlertDialog a = builder.create();
+            a.show();
+
+        }
     }
-}
